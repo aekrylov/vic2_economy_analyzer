b0e6a9b0da3ac977fbdeebd4d6cde2204dc30620
Anton Krylov <anthony.kryloff@gmail.com>
2021-11-29 19:11:52 +0300

    add HPM serfs as RGO pop type

--- src/main/java/org/victoria2/tools/vic2sgea/main/ReportHelpers.java
+++ src/main/java/org/victoria2/tools/vic2sgea/main/ReportHelpers.java
@@ -17,7 +17,7 @@ import java.util.*;
  */
 public class ReportHelpers {
 
-    public static final List<String> POPS_RGO = Arrays.asList("farmers", "labourers", "slaves");
+    public static final List<String> POPS_RGO = Arrays.asList("farmers", "labourers", "slaves", "serfs");
     public static final List<String> POPS_FACTORY = Arrays.asList("craftsmen", "clerks");
     public static final List<String> POPS_ARTISANS = Arrays.asList("artisans");
 
